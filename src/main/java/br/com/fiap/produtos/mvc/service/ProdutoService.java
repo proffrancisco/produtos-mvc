491d4984c6a8283eba5a5656abf997c19a264481
francisco <franads@gmail.com>
2021-05-04 22:03:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/fiap/produtos/mvc/service/ProdutoService.java
@@ -0,0 +1,12 @@
+package br.com.fiap.produtos.mvc.service;
+
+import java.util.List;
+
+import br.com.fiap.produtos.mvc.dto.Produto;
+
+public interface ProdutoService {
+	List<Produto>listaProdutos();
+	Produto salvarProduto(Produto produto);
+	Produto editarProduto(Long id);
+	void excluirProduto(Long id);
+}
