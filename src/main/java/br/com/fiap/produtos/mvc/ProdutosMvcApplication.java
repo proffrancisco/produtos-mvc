30e41bbc0c1f1306b1419e579c93b58a7bee9a5b
unknown <Administrator@L2501MICRO37.fiap.com.br>
2021-05-06 18:54:23 -0300

    teste git maquina fiap

--- src/main/java/br/com/fiap/produtos/mvc/ProdutosMvcApplication.java
+++ src/main/java/br/com/fiap/produtos/mvc/ProdutosMvcApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProdutosMvcApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ProdutosMvcApplication.class, args);
+		
 	}
 
 }
